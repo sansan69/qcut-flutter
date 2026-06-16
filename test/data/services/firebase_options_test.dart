155b6fbea8094f18944464251f173e8193303694
sansan69 <sanjay.deltah@gmail.com>
2026-06-16 22:05:26 +0530

    fix(firebase_options_test): assert expected Android appId and messagingSenderId

--- test/data/services/firebase_options_test.dart
+++ test/data/services/firebase_options_test.dart
@@ -5,6 +5,7 @@ void main() {
   test('defaultFirebaseOptions returns Android options', () {
     final options = DefaultFirebaseOptions.currentPlatform;
     expect(options.projectId, 'appointment-32f4a');
-    expect(options.appId, isNotNull);
+    expect(options.appId, '1:62682041153:android:37d9e2544087052723efc0');
+    expect(options.messagingSenderId, '62682041153');
   });
 }
